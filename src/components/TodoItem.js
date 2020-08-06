94e7b9bcc56e98f7559bec6e99307cd73142e96f
Md Firoj Kabir <firoj.kabir65@gmail.com>
2020-08-06 12:55:44 +0200

    Delete todo list

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -23,6 +23,7 @@ class TodoItem extends Component {
                 <p>
                     <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {'  '}
                     { title }
+                    <button onClick={this.props.delTodo.bind(this, id)} style={btnStyle}>X</button>
                 </p>
             </div>
         )
@@ -31,6 +32,16 @@ class TodoItem extends Component {
 
 TodoItem.propTypes = {
     todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+    background: '#ff0000',
+    color: '#fff',
+    border: 'none',
+    padding: '2px 5px',
+    borderRadius: '50%',
+    cursor: 'pointer',
+    float: 'right'
 }
 
 export default TodoItem
