b527446529a275180fbcb41c58fc3285c620d791
Md Firoj Kabir <firoj.kabir65@gmail.com>
2020-08-06 14:20:38 +0200

    Create about page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './App.css'
 import Header from './components/layout/Header'
@@ -55,13 +56,15 @@ class App extends Component {
 
 	render() {
 		return (
-			<div className="App">
-				<div className="container">
-					<Header />
-					<AddTodo addTodo={this.addTodo} />
-					<Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+			<Router>
+				<div className="App">
+					<div className="container">
+						<Header />
+						<AddTodo addTodo={this.addTodo} />
+						<Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+					</div>
 				</div>
-			</div>
+			</Router>
 		)
 	}
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+    return (
+        <>
+            <h1>About page</h1>
+            <p>This is a Todo App v1.0.0. It is a part of learning React</p>    
+        </>
+    )
+}
+
+export default About
